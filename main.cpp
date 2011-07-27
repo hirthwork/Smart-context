7f203649782332158e202758a35f1f0f51f39aa0
Dmitry Potapov <potapov.d@gmail.com>
2011-07-27 10:31:42 +0400

    more complex main.cpp

--- main.cpp
+++ main.cpp
@@ -11,5 +11,12 @@ int main()
         object = *context.CreateObject();
         std::cout << "leaving scope" << std::endl;
     }
+    std::cout << "creating another context" << std::endl;
+    TContext context;
+    {
+        std::cout << "yet another context created" << std::endl;
+        object = *context.CreateObject();
+        std::cout << "leaving scope again" << std::endl;
+    }
     std::cout << "very last line" << std::endl;
 }
